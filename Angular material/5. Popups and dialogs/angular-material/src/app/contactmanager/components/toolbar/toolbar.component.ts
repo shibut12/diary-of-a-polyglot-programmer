57aad2418353a8c0ac8afa867295f90300882888
Shibu Thannikkunnath <shibu@hotmail.co.in>
2018-01-18 12:00:11 -0500

    Added a snack bar and routing to provide a feedback to user when a new user is added.

--- Angular material/5. Popups and dialogs/angular-material/src/app/contactmanager/components/toolbar/toolbar.component.ts
+++ Angular material/5. Popups and dialogs/angular-material/src/app/contactmanager/components/toolbar/toolbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { MatDialog } from '@angular/material';
+import { MatDialog, MatSnackBar, MatSnackBarRef, SimpleSnackBar } from '@angular/material';
 import { NewContactDialogComponent } from '../new-contact-dialog/new-contact-dialog.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
@@ -10,7 +11,11 @@ import { NewContactDialogComponent } from '../new-contact-dialog/new-contact-dia
 export class ToolbarComponent implements OnInit {
   @Output() toggleSidenav = new EventEmitter<void>();
   
-  constructor(private dialog: MatDialog) { }
+  constructor(
+    private dialog: MatDialog, 
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
@@ -21,8 +26,19 @@ export class ToolbarComponent implements OnInit {
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed', result);
+      if(result){
+        this.openSnackBar("Contact added!", "Navigate")
+        .onAction().subscribe(()=>{
+          this.router.navigate(['/contactmanager', result.id]);
+        })
+      }
     });
+  }
+
+  openSnackBar(message: string, action: string): MatSnackBarRef<SimpleSnackBar>{
+    return this.snackBar.open(message, action, {
+      duration: 5000,
+    })
   }
 
 }
