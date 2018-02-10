518ff5dc87e77fbfbb0cc0981f3749dcd10ebd56
Shibu Thannikkunnath <shibu@hotmail.co.in>
2018-02-10 13:44:12 -0500

    Added a project to show case mocked techniques

---
+++ Unit testing/MOQ/Samples/netcore-moq/Service.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace netcore_moq
+{
+    public interface IMathLib
+    {
+        int Add(List<int> numbers);
+    }
+    public class MathLib:IMathLib
+    {
+        public int Add(List<int> numbers)
+        {
+            int result = 0;
+
+            foreach(int number in numbers)
+            {
+                result += number;
+            }
+
+            return result;
+        }
+    }
+    public class MathService
+    {
+        private IMathLib _mathLib;
+        public MathService(IMathLib mathLib) => _mathLib = mathLib;
+
+        public int Add(List<int> numbers) => _mathLib.Add(numbers: numbers);
+    }
+
+    public class SutMathService
+    {
+        [Fact]
+        public void AddVerifyReturnsSumOfNumbers()
+        {
+        //Given
+        
+        //When
+        
+        //Then
+        Assert.Equal(1,1);
+        }
+    }
+}
