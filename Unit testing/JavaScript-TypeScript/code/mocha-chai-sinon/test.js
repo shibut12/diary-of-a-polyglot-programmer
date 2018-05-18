ceb4e435fd4da407305095e2ffef43de4b0a12a9
Shibu Thannikkunnath <shibu@hotmail.co.in>
2018-05-18 11:12:59 -0400

    Updated test project to include more tests using sinon spy

--- Unit testing/JavaScript-TypeScript/code/mocha-chai-sinon/test.js
+++ Unit testing/JavaScript-TypeScript/code/mocha-chai-sinon/test.js
@@ -7,15 +7,27 @@ var chai = require('chai'),
     chai.should();
 
     describe('sinon tests', function() {
-        var student;
+        var student, schedule;
 
         beforeEach(function(){
             student = {
                 dropClass: function(classId, cb){
                     //do stuff
-                    cb();
+                    if(!!cb.dropClass){
+                        cb.dropClass();
+                    }
+                    else
+                    {
+                        cb();
+                    }
                 }
             };
+
+            schedule = {
+                dropClass: function(){
+                    console.log('class dropped');
+                }
+            }
         })
 
         describe('student.drop class', function(){
@@ -28,5 +40,25 @@ var chai = require('chai'),
                 student.dropClass(1, spy);
                 spy.called.should.be.true; // same as expect(called).to.be.true;
             });
+
+            it('should call the callback and log to the console', function(){
+                function onClassDropped(){
+                    console.log('onClassDropped was called');
+                };
+
+                var spy = sinon.spy(onClassDropped); // Creates a mocked object of onClassDropped, it still call the function (will log data on console)
+                
+                student.dropClass(1, spy);
+
+                spy.called.should.be.true;
+            });
+
+            it('should call the callback even if it\'s a method of an object', function(){
+                sinon.spy(schedule, 'dropClass');
+                student.dropClass(1, schedule);
+                schedule.dropClass.called.should.be.true;
+            });
+
+
         });
     });
